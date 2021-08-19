b932fca1000e78b7ede4fae6548c60b6851b3275
Bruno de Souza Disliler <bruno_disliler@outlook.com>
2021-08-19 19:50:19 -0300

    Adicionando informações ao banco de dados e inserindo link entre as páginas

--- candidato/models.py
+++ candidato/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Registro(models.Model):
+    nome = models.CharField(max_length=100)
+    data_nascimento = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    estado_civil = models.CharField(max_length=50)
+    logradouro = models.CharField(max_length=200)
+    bairro = models.CharField(max_length=50)
+    numero = models.CharField(max_length=50)
+    cidade = models.CharField(max_length=50)
+    estado = models.CharField(max_length=50)
+    cep = models.CharField(max_length=50)
+    tel_1 = models.CharField(max_length=50)
+    tel_2 = models.CharField(max_length=50, blank=True)
+    
+    
