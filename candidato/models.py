8e6f3a22bee2bd99d5ce4eba579e9a8021b55baf
Bruno de Souza Disliler <80045207+BrunoDisliler@users.noreply.github.com>
2021-08-23 11:38:00 -0300

    Link Úteis Funcionais
    Página index fixed

--- candidato/models.py
+++ candidato/models.py
@@ -3,16 +3,18 @@ from django.db import models
 
 class Registro(models.Model):
     nome = models.CharField(max_length=100)
-    data_nascimento = models.CharField(max_length=50)
+    data_nascimento = models.DateField(auto_now=False, auto_now_add=False)
     email = models.EmailField(max_length=100)
     estado_civil = models.CharField(max_length=50)
     logradouro = models.CharField(max_length=200)
     bairro = models.CharField(max_length=50)
-    numero = models.CharField(max_length=50)
+    numero = models.IntegerField(null=True)
     cidade = models.CharField(max_length=50)
     estado = models.CharField(max_length=50)
-    cep = models.CharField(max_length=50)
-    tel_1 = models.CharField(max_length=50)
-    tel_2 = models.CharField(max_length=50, blank=True)
+    cep = models.IntegerField(null=True)
+    tel_1 = models.IntegerField(null=True)
+    tel_2 = models.IntegerField(blank=True)
+
+  
     
     
--- projeto_rh/urls.py
+++ projeto_rh/urls.py
@@ -3,10 +3,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('login/', include('candidato.urls')),
-    path('registro/', include('candidato.urls')),
-    path('experiencias/', include('candidato.urls')),
-    path('cursos/', include('candidato.urls')),
-    path('sobre/', include('candidato.urls')),
+    path('', include('candidato.urls')),
     path('admin/', admin.site.urls),
 ]
