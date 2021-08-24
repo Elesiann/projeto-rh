b5c8a44211c69f0c10345a6d59cdc6161a0eef70
Bruno de Souza Disliler <80045207+BrunoDisliler@users.noreply.github.com>
2021-08-24 09:00:32 -0300

    Incremento
    - Adicionando filtros ao login e registro
    - Importando biblioteca de autenticação django

--- candidato/views.py
+++ candidato/views.py
@@ -1,31 +1,78 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 def login(request):
     if request.method == 'POST':
-        cpf = request.POST['cpf']
+        email = request.POST['email']
         senha = request.POST['password']
-        return redirect('experiencias')
-    else:
-        return render(request, 'index.html')
+        if email == '' or senha == '':
+            print('Os campos email e senha nao podem ficar em branco')
+            return redirect('login')
+        print(email, senha)
+        if User.objects.filter(email=email).exists():
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()
+            user = auth.authenticate(request, username=nome, password=senha)
+            if user is not None:
+                auth.login(request, user)
+                print('Login realizado com sucesso')
+                return redirect('esperiencias')
+    return render(request, 'index.html')
 
 
 def registro(request):
     if request.method == 'POST':
         nome = request.POST['nome']
-        print(nome)
+        data_nascimento = request.POST['datanascimento']
+        email = request.POST['email']
+        estado_civil = request.POST['estadocivil']
+        logradouro = request.POST['logradouro']
+        bairro = request.POST['bairro']
+        numero = request.POST['numerocasa']
+        cidade = request.POST['cidade']
+        estado = request.POST.get('estado')
+        cep = request.POST['cep']
+        tel = request.POST['telefone1']
+        tel_2 = request.POST['telefone2']
+        senha = request.POST['password']
+        senha_2 = request.POST['password2']
+        if not nome.strip():
+            print('O campo nome não pode ficar em branco')
+            return redirect('registro')
+        if not email.strip():
+            print('O campo email não pode ficar em branco')
+            return redirect('registro')
+        if senha != senha_2:
+            print('as senhas não conferem')
+            return redirect('registro')
+        if User.objects.filter(email=email).exists():
+            print('usuário já cadastrado')
+            return redirect('registro')
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        print('usuaário cadastrado com sucesso')
         return redirect('login')
-    else:
-        return render(request, 'registro.html')
+    return render(request, 'registro.html')
+            
+
 
 
 def experiencias(request):
-    return render(request, 'experiencias.html')
-
+    if request.user.is_authenticated:
+        return render(request, 'experiencias.html')
+    else:
+        return redirect('login')
 
 def cursos(request):
-    return render(request, 'cursos.html')
+    if request.user.is_authenticated:
+         return render(request, 'cursos.html')
+    else:
+        return redirect('login')
 
 
 def sobre(request):
-    return render(request, 'sobre.html')
+    if request.user.is_authenticated:
+           return render(request, 'sobre.html')
+    else:
+        return redirect('login')
